c9f3e275aaa259552ede062467f48f3fc297053c
Faisal Kazi <faisal.kazi@welldev.io>
2022-08-08 10:17:08 +0600

    project created

---
+++ PetProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PetProject
+//
+//  Created by Kazi Faisal Arefin on 8/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PetProjectUITests/PetProjectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  PetProjectUITestsLaunchTests.swift
+//  PetProjectUITests
+//
+//  Created by Kazi Faisal Arefin on 8/8/22.
+//
+
+import XCTest
+
+class PetProjectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
